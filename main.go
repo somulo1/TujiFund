809e1b4688f3f3e1d4fe9dec310e57503854a15b
Hilary505 <oduorhilary001@gmail.com>
2024-12-13 16:50:16 +0300

    feat(main.go): Add main function which is the entry  point to our program

--- main.go
+++ main.go
@@ -1,2 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	a"Tujifund/backend/routes"
+)
+
+// main function  begins here
+func main() {
+	http.HandleFunc("/register",a. RegisterHandler)
+	http.HandleFunc("/login", a.LoginHandler)
+
+	fmt.Println("Server is running on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}

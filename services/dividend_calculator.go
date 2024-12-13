89faa7c289726f2b361c049dca6c8b1a607e75c0
FredMunene <munene2050gitonga@gmail.com>
2024-12-14 00:12:33 +0300

    format: comment unnecessary code

--- services/dividend_calculator.go
+++ services/dividend_calculator.go
@@ -1,26 +1,21 @@
 package services
 
-import (
-	"yourproject/models"
-)
-
 func CalculateDividends(totalDividend float64) (map[int]float64, error) {
-	contributions, err := models.GetContributions()
-	if err != nil {
-		return nil, err
-	}
+	// 	contributions, err := models.Contributions
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-	totalContributions := 0.0
-	for _, contribution := range contributions {
-		totalContributions += contribution.Amount
-	}
+	// 	totalContributions := 0.0
+	// 	for _, contribution := range contributions {
+	// 		totalContributions += contribution.Amount
+	// 	}
 
-	dividends := make(map[int]float64)
-	for _, contribution := range contributions {
-		memberShare := (contribution.Amount / totalContributions) * totalDividend
-		dividends[contribution.MemberID] += memberShare
-	}
+		dividends := make(map[int]float64)
+	// 	for _, contribution := range contributions {
+	// 		memberShare := (contribution.Amount / totalContributions) * totalDividend
+	// 		dividends[contribution.MemberID] += memberShare
+	// 	}
 
-	return dividends, nil
+		return dividends, nil
 }
-

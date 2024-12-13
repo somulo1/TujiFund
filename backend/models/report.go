6e7ce59436499dd8f7c01161b66e3faec243720d
FredMunene <munene2050gitonga@gmail.com>
2024-12-14 00:34:23 +0300

    add group , contributions struct and values

--- backend/models/report.go
+++ backend/models/report.go
@@ -4,6 +4,7 @@ import "time"
 
 type Report struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
+	Name   string `json:"file_name" `
 	Type      string    `json:"type"`
 	Data      string    `json:"data"`
 	CreatedAt time.Time `json:"created_at"`
